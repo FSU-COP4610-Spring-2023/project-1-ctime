6a10fad3a935fbf8449b4e8ab12d2cb76e772627
NickRatzlaff <nick.ratzlaff@gmail.com>
2023-01-22 19:40:44 +0000

    Adds basic shell functionality to main.rs

--- project1/project1/src/main.rs
+++ project1/project1/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Write};
+use std::process::Command;
+
+fn main(){
+    loop {
+        //char to use as prompt
+        let prompt_char = "> ";
+        
+        //flush to ensure it prints before read_line
+        print!("{prompt_char}");
+        io::stdout().flush().ok();
+
+        //Read input
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        
+        //Trim into tokens
+        let mut tokens = input.trim().split_whitespace();
+        let mut args = Vec::new();
+
+        //Separate tokens into command and args
+        let command= tokens.next().unwrap();
+        for token in tokens{
+            args.push(token);
+        }
+
+        //Spawn a new thread and execute the command with arguments
+        let mut child = Command::new(command)
+            .args(args)
+            .spawn()
+            .unwrap();
+
+        child.wait().ok();
+    }
 }
